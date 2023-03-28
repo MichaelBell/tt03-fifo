d57a812c4f8bd47f8bc250cc50652b016afad58b
Mike Bell <mike@mercuna.com>
2023-03-29 00:22:50 +0100

    Try increasing depth to 32

--- src/fifo_top.v
+++ src/fifo_top.v
@@ -10,7 +10,7 @@
    limitations under the License.
 */
 
-module MichaelBell_6bit_fifo (
+module MichaelBell_6bit_fifo #( parameter DEPTH_BITS = 5 ) (
   input [7:0] io_in,
   output [7:0] io_out
 );
@@ -25,26 +25,26 @@ module MichaelBell_6bit_fifo (
     wire [5:0] data_in = io_in[7:2];
 
     // Fifo data
-    reg [5:0] fifo_data[0:15];
-    reg [3:0] write_addr;
-    reg [3:0] read_addr;
-    wire [3:0] next_read_addr = read_addr + 1;
+    reg [5:0] fifo_data[0:(1 << DEPTH_BITS) - 1];
+    reg [DEPTH_BITS-1:0] write_addr;
+    reg [DEPTH_BITS-1:0] read_addr;
+    wire [DEPTH_BITS-1:0] next_read_addr = read_addr + 1;
     reg empty_n;
 
     // Simple outputs
-    assign io_out[0] = !clk;
+    assign io_out[0] = clk;
     assign io_out[1] = empty_n;
 
     // Data out, registered so that popped value
     // is presented on outputs when pop occurs.
     reg [5:0] data_out;
     assign io_out[7:2] = data_out;
-    wire [3:0] peek_addr = read_addr + peek;
+    wire [DEPTH_BITS-1:0] peek_addr = read_addr + peek;
 
     // Generate all writes to the FIFO data.
     genvar i;
     generate
-        for (i = 0; i < 16; i = i+1) begin
+        for (i = 0; i < (1 << DEPTH_BITS); i = i+1) begin
             always @(posedge clk)
             begin
                 if (!reset_n) begin
